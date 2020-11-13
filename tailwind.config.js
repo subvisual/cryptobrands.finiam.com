b69532e885b085689019613feac5229da9547b09
Rafael Ferreira <rafaelgfest6@gmail.com>
2020-11-13 21:23:32 +0000

    structure & base
    - add fonts
    - add stucture
    - add projects
    - css & basics

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,30 @@ module.exports = {
     mode: "all",
     content: ["./src/**/*.js", "./src/**/*.njk", "./src/**/*.liquid", "./src/**/*.ts"],
   },
+  theme: {
+    fontSize: {
+      tiny: '0.8rem',
+      base: '1rem',
+    },
+    maxWidth: {
+      'xl': '1280px',
+    },
+    colors: {
+      purple: {
+        dark:'#09001A',
+      },
+      white: '#FFFFFF',
+      green: '#5EF38B',
+      gray: {
+        default: '#757575',
+        lighter: '#E1E1E1',
+        lightest: '#FCFCFC',
+      }
+    },
+    extend: {
+      gap: {
+      '5': '1.4rem',
+      }
+    }
+  }
 };
