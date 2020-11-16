c083685725f89dcd9fa3f18e35e252746c889087
Rafael Ferreira <rafaelgfest6@gmail.com>
2020-11-16 16:17:35 +0000

    Add graphic structure (#1)
    * structure & base
    
    - add fonts
    - add stucture
    - add projects
    - css & basics
    
    * fix mobile & good practices Lightouse
    
    * add about & minor fixes
    
    - add about page
    - minor fixes css
    
    * fix crypto brands -> CryptoBrands
    
    * fix z-index click
    
    * fix tests
    
    run prettier
    
    * add br & migrate classes o tailwind syntax
    
    * Add .DS_Store to gitignore
    
    * fix tests & add a new line on index.css
    
    * structure & base
    
    - add fonts
    - add stucture
    - add projects
    - css & basics
    
    * fix mobile & good practices Lightouse
    
    * add about & minor fixes
    
    - add about page
    - minor fixes css
    
    * fix crypto brands -> CryptoBrands
    
    * fix z-index click
    
    * fix tests
    
    run prettier
    
    * add br & migrate classes o tailwind syntax
    
    * fix tests & add a new line on index.css
    
    * remove ds store
    
    * remove ds store!
    
    * add loading="lazy" to all images
    
    * add mailto & twitter link
    
    Co-authored-by: Francisco Sousa <josesousa9000@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,48 @@
 module.exports = {
   purge: {
     mode: "all",
-    content: ["./src/**/*.js", "./src/**/*.njk", "./src/**/*.liquid", "./src/**/*.ts"],
+    content: [
+      "./src/**/*.js",
+      "./src/**/*.njk",
+      "./src/**/*.liquid",
+      "./src/**/*.ts",
+    ],
+  },
+  theme: {
+    scale: {
+      101: "1.01",
+    },
+    fontSize: {
+      tiny: "0.8rem",
+      base: "1rem",
+      md: "1.4rem",
+    },
+    maxWidth: {
+      950: "950px",
+      xl: "1280px",
+    },
+    colors: {
+      purple: {
+        dark: "#09001A",
+      },
+      white: "#FFFFFF",
+      green: "#5EF38B",
+      gray: {
+        default: "#757575",
+        lighter: "#E1E1E1",
+        lightest: "#FCFCFC",
+      },
+    },
+    extend: {
+      gap: {
+        5: "1.4rem",
+      },
+      textOpacity: {
+        80: "0.80",
+      },
+      backgroundOpacity: {
+        95: "0.95",
+      },
+    },
   },
 };
