83cfcc0c37a0408211ff99cc1733cdf4342c5103
Rafael Ferreira <rafaelgfest6@gmail.com>
2020-11-16 12:53:33 +0000

    add br & migrate classes o tailwind syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,16 @@ module.exports = {
     ],
   },
   theme: {
+    scale: {
+      '101': '1.01',
+    },
     fontSize: {
       tiny: "0.8rem",
       base: "1rem",
       md: "1.4rem",
     },
     maxWidth: {
+      '950': "950px",
       xl: "1280px",
     },
     colors: {
