d1853480e1d13a53d233734c934da2881000abbc
Francisco Sousa <josesousa9000@gmail.com>
2021-01-21 10:20:26 +0000

    Implement responsive image helpers and setup subfont (#24)
    - Add responsive image helpers for that performance BOOST
    - Setup subfont to handle and optimize fonts

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,30 +1,31 @@
 module.exports = {
   mount: {
     _output: "/",
-    src: "/_dist_",
+    static: "/",
+    "src/assets": "/assets",
   },
+
   plugins: [
     "@snowpack/plugin-postcss",
-    ["@snowpack/plugin-run-script", { cmd: "eleventy", watch: "$1 --watch" }],
-    [
-      "@snowpack/plugin-webpack",
-      {
-        extendConfig: (config) => ({
-          ...config,
-          optimization: {
-            ...config.optimization,
 
-            runtimeChunk: false,
-            splitChunks: {
-              chunks: "async",
-            },
-          },
-        }),
-      },
+    [
+      "@snowpack/plugin-run-script",
+      { cmd: "eleventy", watch: "$1 --watch --silent" },
     ],
   ],
+
   devOptions: {
     open: "none",
     hmrDelay: 500,
+  },
+
+  packageOptions: {
+    external: ["fonts"],
+  },
+
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: "es2017",
   },
 };
