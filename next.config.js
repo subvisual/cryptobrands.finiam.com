973c337e8ac741c540cdf2afa15c6f78c58d60fd
Francisco Sousa <francisco.sousa@hey.com>
2021-02-12 23:59:37 +0000

    Add caching headers to the home page

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@ const withOptimizedImages = require("next-optimized-images");
 
 module.exports = withOptimizedImages({
   target: "experimental-serverless-trace",
+
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "max-age=1800",
+          },
+        ],
+      },
+    ];
+  },
 });
