4e56bf8e2441e8ab66b9aa4a83f265a0a38321a5
Francisco Sousa <josesousa9000@gmail.com>
2021-03-11 16:09:41 +0000

    Make the about page modal accessible (#30)
    Use reach ui (around 6kb) to make the modal accessible.

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import Head from "next/head";
+import Dialog from "@reach/dialog";
 import AboutInner from "./AboutInner";
 
-export default function AboutModal({ onClose }) {
+export default function AboutModal({ onDismiss }) {
   return (
-    <>
+    <Dialog
+      onDismiss={onDismiss}
+      className="fixed w-full h-full p-0 m-0 z-50 bg-purple-dark bg-opacity-95"
+      aria-label="About page content"
+    >
       <Head>
         <title>CryptoBrands - About</title>
       </Head>
 
-      <div
-        className="w-full h-full fixed overflow-y-auto bg-purple-dark bg-opacity-95 z-50 px-5"
-        data-js="about"
-        role="dialog"
-        aria-label="About this project"
-        aria-modal="true"
-      >
-        <div className="max-w-xl mx-auto py-20">
-          <button
-            className="text-tiny text-white cursor-pointer text-opacity-80 mb-10 inline-block hover:text-opacity-100"
-            onClick={onClose}
-            type="button"
-          >
-            ← Back to projects
-          </button>
+      <div className="max-w-xl mx-auto py-20">
+        <button
+          className="text-tiny text-white cursor-pointer text-opacity-80 mb-10 inline-block hover:text-opacity-100"
+          onClick={onDismiss}
+          type="button"
+        >
+          ← Back to projects
+        </button>
 
-          <AboutInner />
-        </div>
+        <AboutInner />
       </div>
-    </>
+    </Dialog>
   );
 }
